f053d74b7efbfedb80e6cc58ea234ea525282610
Fio Leukert <leukert.fio@gmail.com>
2022-05-26 17:09:50 +0200

    ping isn't a flashbang anymore

--- autoload/GameData.gd
+++ autoload/GameData.gd
@@ -8,7 +8,7 @@ const ARENA_ORDER = [
 	"res://src/level/arenas/arena2-1.tscn"
 ]
 
-var next_arena = 0 # var used in the main menu button selection to tell the arena controller which arena to load first
+var next_arena = 3 # var used in the main menu button selection to tell the arena controller which arena to load first
 
 
 var allow_grappling_hook = true
--- src/PlayerPing.gd
+++ src/PlayerPing.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
 
-onready var illumination_sprite = $Illum640000
+onready var ping_body = $PingBody
+onready var ping_pulse = $PingPulse
+onready var timer = $FlashbangTimer
+onready var tween = $Tween
 
+const TARGET_SCALE_BODY = 2.5
+const TARGET_SCALE_PULSE = 100
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	illumination_sprite.scale = Vector2(100, 100)
+	tween.interpolate_property(ping_body, "scale", Vector2(1, 1), Vector2(TARGET_SCALE_BODY, TARGET_SCALE_BODY), 1.0)
+	tween.interpolate_property(ping_pulse, "scale", Vector2(1, 1), Vector2(TARGET_SCALE_PULSE, TARGET_SCALE_PULSE), 5.0)
+	tween.start()
+
+
 
 
 func _on_FlashbangTimer_timeout() -> void:
-	illumination_sprite.scale = Vector2(5,5)
+	ping_pulse.visible = false
+
+
